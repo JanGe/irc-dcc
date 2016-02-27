82cdb5bb80ff6f4ba6843eda7ff29f51c2fda7a4
Jan Gerlinger <git@jangerlinger.de>
2016-02-27 20:26:24 +0100

    Remove unnecessary type restrictions

--- src/Network/IRC/DCC/FileTransfer.hs
+++ src/Network/IRC/DCC/FileTransfer.hs
@@ -7,7 +7,6 @@ import           Network.IRC.DCC.Internal
 
 import           Control.Error
 import           Control.Monad              (unless)
-import           Control.Monad.IO.Class     (MonadIO)
 import           Data.Binary.Put            (putWord32be, runPut)
 import           Data.ByteString.Char8      (ByteString, length, null)
 import qualified Data.ByteString.Lazy.Char8 as Lazy (toStrict)
@@ -19,52 +18,58 @@ import           System.IO                  (BufferMode (NoBuffering),
 import           System.IO.Streams          (OutputStream, withFileAsOutputExt,
                                              write)
 
-type Handle = OutputStream ByteString
+type Output = OutputStream ByteString
 
-acceptFile :: Offer
+acceptFile :: Integral a
+           => Offer
            -> (PortNumber -> ExceptT String IO ())
-           -> (FileOffset -> IO ())
+           -> (a -> IO ())
            -> ExceptT String IO ()
-acceptFile (Offer tt f) = downloadFile tt (fileName f) 0
+acceptFile (Offer tt f) =
+    download (fileName f) WriteMode 0 tt
 
-resumeFile :: AcceptResume
+resumeFile :: Integral a
+           => AcceptResume
            -> (PortNumber -> ExceptT String IO ())
-           -> (FileOffset -> IO ())
+           -> (a -> IO ())
            -> ExceptT String IO ()
-resumeFile (AcceptResume tt f pos) = downloadFile tt (fileName f) pos
+resumeFile (AcceptResume tt f pos) =
+    download (fileName f) AppendMode (fromIntegral pos) tt
 
-downloadFile :: TransferType
-             -> Path Rel File
-             -> FileOffset
-             -> (PortNumber -> ExceptT String IO ())
-             -> (FileOffset -> IO ())
-             -> ExceptT String IO ()
-downloadFile tt fn pos onListen onChunk =
-    withSocket tt onListen (\ con ->
-        withFileAsOutputExt (fromRelFile fn) (fileMode pos) NoBuffering (\ f ->
-            downloadToFile f onChunk pos con))
+download :: Integral a
+         => Path Rel File
+         -> IOMode
+         -> a
+         -> TransferType
+         -> (PortNumber -> ExceptT String IO ())
+         -> (a -> IO ())
+         -> ExceptT String IO ()
+download fn mode pos tt onListen onChunk =
+    withSocket tt onListen $
+        toFile (fromRelFile fn) . stream pos onChunk
   where withSocket (Active i p) = withActiveSocket i p
         withSocket (Passive i _) = withPassiveSocket i
+        toFile f = withFileAsOutputExt f mode NoBuffering
 
-fileMode :: (Eq a, Num a) => a -> IOMode
-fileMode 0 = WriteMode
-fileMode _ = AppendMode
+stream :: Integral a
+       => a
+       -> (a -> IO ())
+       -> Socket
+       -> Output
+       -> IO ()
+stream pos onChunk sock h =
+  do buf <- recv sock (4096 * 1024)
+     unless (null buf) $
+            do let l = fromIntegral (length buf)
+               onChunk l
+               let pos' = pos + l
+               sendPosition sock pos'
+               Just buf `write` h
+               stream pos' onChunk sock h
 
-downloadToFile :: Handle -> (FileOffset -> IO a) -> FileOffset -> Socket
-               -> IO ()
-downloadToFile file onChunk size sock =
-  do buffer <- recv sock $ 4096 * 1024
-     unless (null buffer) $
-            do let received = fromIntegral (length buffer)
-               onChunk received
-               let receivedTotal = size + received
-               sendNumReceived sock receivedTotal
-               Just buffer `write` file
-               downloadToFile file onChunk receivedTotal sock
-
-sendNumReceived :: Socket -> FileOffset -> IO ()
-sendNumReceived sock num = sendAll sock $ toNetworkByteOrder num
+sendPosition :: Integral a => Socket -> a -> IO ()
+sendPosition sock = sendAll sock . toNetworkByteOrder
 
 -- TODO Simplify
-toNetworkByteOrder :: FileOffset -> ByteString
+toNetworkByteOrder :: Integral a => a -> ByteString
 toNetworkByteOrder = Lazy.toStrict . runPut . putWord32be . fromIntegral
