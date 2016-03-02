54506a671a94118f07b8a4d6b0b1f426f4a03aa4
Jan Gerlinger <git@jangerlinger.de>
2016-03-02 17:10:52 +0100

    Support older servers not sending file size

--- src/Network/IRC/DCC/Internal.hs
+++ src/Network/IRC/DCC/Internal.hs
@@ -10,7 +10,7 @@ import           Data.ByteString.Char8            (ByteString, pack)
 import qualified Data.ByteString.UTF8             as UTF8 (toString)
 import           Data.IP                          (IPv4, fromHostAddress,
                                                    toHostAddress)
-import           Data.List                        (intersperse)
+import           Data.Monoid                      ((<>))
 import           Data.Word                        (Word64)
 import           Network.IRC.CTCP                 (CTCPByteString, decodeCTCP,
                                                    encodeCTCP)
@@ -64,7 +64,7 @@ data TransferType
 
 -- | Properties of the file to be transfered
 data FileMetadata = FileMetadata { fileName :: Path Rel File
-                                 , fileSize :: FileOffset }
+                                 , fileSize :: Maybe FileOffset }
                     deriving (Eq, Show)
 
 -- | An identifier for knowing which negotiation a request belongs to
@@ -77,25 +77,22 @@ runParser :: Parser a -> CTCPByteString -> Either String a
 runParser p = parseOnly p . decodeCTCP
 
 decodeService :: Parser Service
-decodeService = do "DCC"
-                   space
-                   choice [ do "CHAT"
-                               space
+decodeService = do "DCC "
+                   choice [ do "CHAT "
                                m <- decodeChatOffer
                                return $ Messaging m
-                          , do "SEND"
-                               space
+                          , do "SEND "
                                o <- decodeFileOffer
                                return $ FileTransfer o
                           ]
 
 encodeService :: Service -> CTCPByteString
 encodeService (Messaging m) = encodeCTCP $
-    concatSpace [ "DCC CHAT"
-                , encodeChatOffer m ]
+    "DCC CHAT "
+ <> encodeChatOffer m
 encodeService (FileTransfer o) = encodeCTCP $
-    concatSpace [ "DCC SEND"
-                , encodeFileOffer o ]
+    "DCC SEND "
+ <> encodeFileOffer o
 
 decodeChatClose :: Parser CloseChat
 decodeChatClose = do "DCC CLOSE"
@@ -106,31 +103,19 @@ encodeChatClose :: CloseChat -> CTCPByteString
 encodeChatClose _ = encodeCTCP "DCC CLOSE"
 
 decodeChatOffer :: Parser ChatProtocol
-decodeChatOffer = choice [ do "chat"
-                              space
-                              i <- decodeIpBigEndian
-                              space
-                              p <- decodeTcpPort
+decodeChatOffer = choice [ do "chat "
+                              (i, p) <- decodeSocket
                               endOfInput
                               return $ Chat i p
-                         , do "wboard"
-                              space
-                              i <- decodeIpBigEndian
-                              space
-                              p <- decodeTcpPort
+                         , do "wboard "
+                              (i, p) <- decodeSocket
                               endOfInput
                               return $ Whiteboard i p
                          ]
 
 encodeChatOffer :: ChatProtocol -> ByteString
-encodeChatOffer (Chat i p) =
-   concatSpace [ "chat"
-               , encodeIpBigEndian i
-               , encodeTcpPort p ]
-encodeChatOffer (Whiteboard i p) =
-   concatSpace [ "wboard"
-               , encodeIpBigEndian i
-               , encodeTcpPort p ]
+encodeChatOffer (Chat i p) = "chat " <> encodeSocket i p
+encodeChatOffer (Whiteboard i p) = "wboard " <> encodeSocket i p
 
 decodeFileOffer :: Parser Offer
 decodeFileOffer = do fn <- decodeFileName
@@ -138,8 +123,11 @@ decodeFileOffer = do fn <- decodeFileName
                      i <- decodeIpBigEndian
                      space
                      choice [ do p <- decodeTcpPort
-                                 space
-                                 fs <- decodeFileOffset
+                                 fs <- choice [ do space
+                                                   fs <- decodeFileOffset
+                                                   return $ Just fs
+                                              , return Nothing
+                                              ]
                                  endOfInput
                                  return (Offer (Active i p)
                                                (FileMetadata fn fs))
@@ -150,21 +138,21 @@ decodeFileOffer = do fn <- decodeFileName
                                  t <- decodeToken
                                  endOfInput
                                  return (Offer (Passive i t)
-                                               (FileMetadata fn fs))
+                                               (FileMetadata fn (Just fs)))
                             ]
 
 encodeFileOffer :: Offer -> ByteString
 encodeFileOffer (Offer (Active i p) (FileMetadata fn fs)) =
-   concatSpace [ encodeFileName fn
-               , encodeIpBigEndian i
-               , encodeTcpPort p
-               , encodeFileOffset fs ]
+    encodeFileName fn
+ <> " " <> encodeIpBigEndian i
+ <> " " <> encodeTcpPort p
+ <> appendSpacedIfJust (encodeFileOffset <$> fs)
 encodeFileOffer (Offer (Passive i t) (FileMetadata fn fs)) =
-   concatSpace [ encodeFileName fn
-               , encodeIpBigEndian i
-               , "0"
-               , encodeFileOffset fs
-               , encodeToken t ]
+    encodeFileName fn
+ <> " " <> encodeIpBigEndian i
+ <> " 0"
+ <> appendSpacedIfJust (encodeFileOffset <$> fs)
+ <> " " <> encodeToken t
 
 decodeTryResume :: Offer -> Parser TryResume
 decodeTryResume (Offer tt f) =
@@ -192,21 +180,20 @@ decodeTryResume (Offer tt f) =
 
 encodeTryResume :: TryResume -> CTCPByteString
 encodeTryResume (TryResume (Active _ p) (FileMetadata fn _) pos) =
-    encodeCTCP $ concatSpace [ "DCC RESUME"
-                             , encodeFileName fn
-                             , encodeTcpPort p
-                             , encodeFileOffset pos ]
+    encodeCTCP $ "DCC RESUME "
+              <> encodeFileName fn
+              <> " " <> encodeTcpPort p
+              <> " " <> encodeFileOffset pos
 encodeTryResume (TryResume (Passive _ t) (FileMetadata fn _) pos) =
-    encodeCTCP $ concatSpace [ "DCC RESUME"
-                             , encodeFileName fn
-                             , "0"
-                             , encodeFileOffset pos
-                             , encodeToken t ]
+    encodeCTCP $ "DCC RESUME "
+              <> encodeFileName fn
+              <> " 0 "
+              <> encodeFileOffset pos
+              <> " " <> encodeToken t
 
 decodeAcceptResume :: TryResume -> Parser AcceptResume
 decodeAcceptResume (TryResume tt f _) =
-    do "DCC ACCEPT"
-       space
+    do "DCC ACCEPT "
        fn' <- decodeFileName
        when (fn' /= fileName f) $
          fail "File name for accepting resume didn't match file name in offer."
@@ -228,30 +215,26 @@ decodeAcceptResume (TryResume tt f _) =
 
 encodeAcceptResume :: AcceptResume -> CTCPByteString
 encodeAcceptResume (AcceptResume (Active _ p) (FileMetadata fn _) pos) =
-    encodeCTCP $ concatSpace [ "DCC ACCEPT"
-                             , encodeFileName fn
-                             , encodeTcpPort p
-                             , encodeFileOffset pos ]
+    encodeCTCP $ "DCC ACCEPT "
+              <> encodeFileName fn
+              <> " " <> encodeTcpPort p
+              <> " " <> encodeFileOffset pos
 encodeAcceptResume (AcceptResume (Passive _ t) (FileMetadata fn _) pos) =
-    encodeCTCP $ concatSpace [ "DCC ACCEPT"
-                             , encodeFileName fn
-                             , "0"
-                             , encodeFileOffset pos
-                             , encodeToken t ]
+    encodeCTCP $ "DCC ACCEPT "
+              <> encodeFileName fn
+              <> " 0 "
+              <> encodeFileOffset pos
+              <> " " <> encodeToken t
 
 decodeOfferSink :: AcceptResume -> Parser (Maybe OfferSink)
 decodeOfferSink (AcceptResume tt@(Passive _ t) f _) =
-    do "DCC SEND"
-       space
+    do "DCC SEND "
        fn <- decodeFileName
        when (fn /= fileName f) $
          fail "File name for accepting resume didn't match file name in offer."
        space
-       i <- decodeIpBigEndian
-       space
-       p <- decodeTcpPort
-       space
-       string (encodeFileOffset (fileSize f))
+       (i, p) <- decodeSocket
+       string (appendSpacedIfJust (encodeFileOffset <$> fileSize f))
        space
        string (encodeToken t)
        endOfInput
@@ -260,13 +243,22 @@ decodeOfferSink _ = return Nothing
 
 encodeOfferSink :: OfferSink -> Maybe CTCPByteString
 encodeOfferSink (OfferSink (Passive _ t) (FileMetadata fn fs) i p) = Just $
-    encodeCTCP $ concatSpace [ "DCC SEND"
-                             , encodeFileName fn
-                             , encodeIpBigEndian i
-                             , encodeTcpPort p
-                             , encodeFileOffset fs
-                             , encodeToken t ]
+    encodeCTCP $ "DCC SEND "
+              <> encodeFileName fn
+              <> " " <> encodeSocket i p
+              <> appendSpacedIfJust (encodeFileOffset <$> fs)
+              <> " " <> encodeToken t
 encodeOfferSink _ = Nothing
+
+decodeSocket :: Parser (IPv4, PortNumber)
+decodeSocket =
+    do i <- decodeIpBigEndian
+       space
+       p <- decodeTcpPort
+       return (i, p)
+
+encodeSocket :: IPv4 -> PortNumber -> ByteString
+encodeSocket i p = encodeIpBigEndian i <> " " <> encodeTcpPort p
 
 decodeFileName :: Parser (Path Rel File)
 decodeFileName = do name <- "\"" *> takeWhile1 (/= '"') <* "\""
@@ -304,6 +296,10 @@ decodeToken = Token <$> takeByteString
 encodeToken :: Token -> ByteString
 encodeToken (Token t) = t
 
+appendSpacedIfJust :: Maybe ByteString -> ByteString
+appendSpacedIfJust (Just x) = " " <> x
+appendSpacedIfJust Nothing = ""
+
 parseBoundedInteger :: Integer -> Integer -> Parser Integer
 parseBoundedInteger lower upper = do
     num <- decimal
@@ -311,9 +307,6 @@ parseBoundedInteger lower upper = do
          fail ("Failed to parse " ++ show num ++ ", not in range [" ++
                show lower ++ ", " ++ show upper ++ "].")
     return num
-
-concatSpace :: [ByteString] -> ByteString
-concatSpace = mconcat . intersperse " "
 
 fromBigEndianIp :: Integer -> IPv4
 fromBigEndianIp = fromHostAddress . byteSwap32 . fromIntegral
